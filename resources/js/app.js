6ec5176d660ffc05c19c8f5ce92ae4a294d331cf
Marc Hershey <marchershey@users.noreply.github.com>
2021-05-07 06:32:22 -0400

    initial commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+window.axios = require("axios")
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest"
+
+window.Vue = require("vue").default
+const files = require.context("./", true, /\.vue$/i)
+files.keys().map(key =>
+    Vue.component(key.split("/").pop().split(".")[0], files(key).default)
+)
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: "#app"
+})
